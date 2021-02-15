e41ad30cd3e12a6f28e34a6b2717817598c8412b
Peijun Ma <peijun.ma@protonmail.com>
2021-02-14 22:51:17 -0500

    Implement a dummy subscribe end point

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,25 @@
 #![allow(clippy::unit_arg)]
-use rocket::Config;
+use rocket::{request::Form, Config};
 
 #[macro_use]
 extern crate rocket;
 
+#[derive(FromForm)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 #[get("/health_check")]
 fn health_check() {}
+
+#[post("/subscriptions", data = "<_form>")]
+fn subscribe(_form: Form<FormData>) {}
 
 pub fn app(port: u16) -> rocket::Rocket {
     let config = Config {
         port,
         ..Config::default()
     };
-    rocket::custom(config).mount("/", routes![health_check])
+    rocket::custom(config).mount("/", routes![health_check, subscribe])
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
-use std::{io, net::TcpListener};
+use std::net::TcpListener;
 
+use reqwest::StatusCode;
 use rocket::tokio;
 use zero2prod::app;
 
@@ -19,9 +20,56 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> io::Result<u16> {
-    let port = TcpListener::bind("127.0.0.1:0")?.local_addr()?.port();
+#[rocket::async_test]
+async fn subscribe_returns_a_200_on_valid_form_data() {
+    let port = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(&format!("http://127.0.0.1:{}/subscriptions", port))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+}
+
+#[rocket::async_test]
+async fn subscribe_returns_a_422_when_data_is_missing() {
+    let port = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("http://127.0.0.1:{}/subscriptions", port))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            StatusCode::UNPROCESSABLE_ENTITY,
+            response.status(),
+            "The API did not fail with Unprocessable Entity when the payload was {}",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> u16 {
-    let port = TcpListener::bind("127.0.0.1:0").and_then(|s|s.local_addr()).map(|a| a.port()).expect("Could not get port.");
+    let port = TcpListener::bind("127.0.0.1:0")
+        .and_then(|s| s.local_addr())
+        .map(|a| a.port())
+        .expect("Could not get port.");
     let _ = tokio::spawn(app(port).launch());
+    port
 }
